bce61befc2f62c17c140c49ed1a24962c0e0f124
Ivan Wallis <iwallis@gmail.com>
2023-03-30 09:28:54 -0700

    v0.2.0 support
    Closes #4.
    
    Signed-off-by: Ivan Wallis <799752+zosocanuck@users.noreply.github.com>

--- Formula/sigscan.rb
+++ Formula/sigscan.rb
@@ -1,32 +1,32 @@
 class Sigscan < Formula
   desc "Inspect container images and other artifacts for signatures"
   homepage "https://github.com/venafi/sigscan"
-  url "https://github.com/venafi/sigscan/archive/refs/tags/v0.1.0.tar.gz"
-  version "0.1.0"
+  url "https://github.com/venafi/sigscan/archive/refs/tags/v0.2.0.tar.gz"
+  version "0.2.0"
   license "Apache-2.0"
-  head "https://github.com/venafi/sigscan.git", tag: "v0.1.0"
+  head "https://github.com/venafi/sigscan.git", tag: "v0.2.0"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/Venafi/sigscan/releases/download/v#{version}/sigscan-darwin-amd64"
-      sha256 "6365128a172eb19053eee81d274724e96a29640a9366230e90213183f551e93b"
+      sha256 "42a7f628b78db736f0530f99e8453bb2628021bf464e2ca54dc7993bd59b9d2d"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/Venafi/sigscan/releases/download/v#{version}/sigscan-darwin-arm64"
-      sha256 "8022f6350489c03400ce6cc441d864c747f6d645df47ff60c854e05c3c4dd24e"
+      sha256 "c93cf85b18e1ba8596fe105ef156f95d73868e8ecfa0b13178e95558aee86896"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/Venafi/sigscan/releases/download/v#{version}/sigscan-linux-amd64"
-      sha256 "53c84ff2888f509477cafd903402f8afe51c81912afc451b26bfba42e9107264"
+      sha256 "15110854adc85c4ecf783694c308a798945473e1a631a1f3b23099e63841f637"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/Venafi/sigscan/releases/download/v#{version}/sigscan-linux-arm64"
-      sha256 "bfbc28191d0717344f2e46ef10e0609272b87e74bcb9e9ee459bb0b26faea0ba"
+      sha256 "35074ec7d85452c08e15f853f67f2e252e610565481a699f0436f046931b2e6c"
     end
   end
 
@@ -37,6 +37,6 @@ class Sigscan < Formula
   end
 
   test do
-    system "#{bin}/sigscan", "-h"
+    system "#{bin}/sigscan", "version"
   end
 end
