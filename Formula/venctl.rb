c0e46aae36bb3baef53652b51b8a746050309399
Mattias Gees <mattias.gees@gmail.com>
2024-04-15 15:23:13 +0100

    Upgrade venctl to 1.8.0
    Signed-off-by: Mattias Gees <mattias.gees@gmail.com>

--- Formula/venctl.rb
+++ Formula/venctl.rb
@@ -2,26 +2,26 @@ class Venctl < Formula
   desc "Venafi CLI serves as an alternative to the Venafi Control Plane web interface"
   homepage "https://docs.venafi.cloud/vaas/venctl/c-venctl-overview"
   url "https://docs.venafi.cloud/vaas/venctl/c-venctl-releases"
-  version "1.7.0"
+  version "1.8.0"
   on_macos do
     if Hardware::CPU.intel?
       url "https://dl.venafi.cloud/venctl/#{version}/venctl-darwin-amd64.zip"
-      sha256 "86d4a11ecd582b94d45e35de66112fa90ee154a0c1c547f0389832a3c7453960"
+      sha256 "4692d780be197b47a94caac269dd3e3a248bcf3ae246cb9a9c23007d810c12a7"
     end
     if Hardware::CPU.arm?
       url "https://dl.venafi.cloud/venctl/#{version}/venctl-darwin-arm64.zip"
-      sha256 "eb2fde0b9f45d9be0d4debb0d12ade9c8b956a61bcb2c0b65a677f38135b9d58"
+      sha256 "66a59e1fbe31ac941782f35588651a1f8e47555107d7e47a1d5afb70d678ae32"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://dl.venafi.cloud/venctl/#{version}/venctl-linux-amd64.zip"
-      sha256 "3455b982fed40d80a40759c42bf8e5145bdc0ec3121744652f73a05d87c2b088"
+      sha256 "c4a49eb57c337e93c33a004e8cda3e48d4ad27e3d6beb0d04c54c217b23b6768"
     end
     if Hardware::CPU.arm?
       url "https://dl.venafi.cloud/venctl/#{version}/venctl-linux-arm64.zip"
-      sha256 "154ff31bebe9bf8019a94489cb80f34e4161eed3461312c9f6b805adaedefa53"
+      sha256 "e536fc37c3ed2339780a5f272a49d2fb5a1a5005c770c8c18ca0093dd57f866a"
     end
   end
 
