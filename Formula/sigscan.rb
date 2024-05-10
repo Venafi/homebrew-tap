bcd1ecd98bc29d90d8d7ea03d31f710e904b076b
Ivan Wallis <iwallis@gmail.com>
2024-05-10 14:13:15 -0700

    audit fix

--- Formula/sigscan.rb
+++ Formula/sigscan.rb
@@ -7,14 +7,14 @@ class Sigscan < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/Venafi/sigscan/releases/download/v#{version}/sigscan-darwin-amd64"
+      url "https://github.com/Venafi/sigscan/releases/download/v0.3.0/sigscan-darwin-amd64"
       sha256 "a721fddfd54c68d7afeeb811df443d7e90483ec76654b66d8f1020c53b4c43ba"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/Venafi/sigscan/releases/download/v#{version}/sigscan-linux-amd64"
+      url "https://github.com/Venafi/sigscan/releases/download/v0.3.0/sigscan-linux-amd64"
       sha256 "0005bf25ac188e13f88499b38fca37089ad5648451b445833b468e2c830ac03f"
     end
   end
